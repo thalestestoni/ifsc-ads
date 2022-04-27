4b84bb1b107f9994c4a8179f1183587645b86785
Thales Flores Testoni <thales.testoni@havan.com.br>
2022-04-26 21:12:32 -0300

    Criado construtor e métodos na classe Livro

--- programacao-internet-2/aula03/Livro.php
+++ programacao-internet-2/aula03/Livro.php
@@ -1,9 +1,48 @@
 <?php
-    public class Livro {
+    class Livro {
         private $titulo;
         private $isbn;
         private $nPaginas;
         private $anoPublicacao;
         private $numEdicao;
+        private $autor;
+        private $editora;
+
+        function __construct($titulo, $isbn, $nPaginas, $anoPublicacao, $numEdicao)
+        {
+            $this->titulo = $titulo;
+            $this->isbn = $isbn;
+            $this->nPaginas = $nPaginas;
+            $this->anoPublicacao = $anoPublicacao;
+            $this->numEdicao = $numEdicao;
+        }
+
+        function getTitulo(){
+            return $this->titulo;
+        }
+
+        function getIsbn() {
+            return $this->isbn;
+        }
+
+        function getNPaginas() {
+            return $this->nPaginas;
+        }
+
+        function getAnoPublicacao() {
+            return $this->anoPublicacao;
+        }
+
+        function getNumEdicao() {
+            return $this->numEdicao;
+        }
+
+        function getAutor() {
+            //TODO
+        }
+
+        function getEditora() {
+            //TODO
+        }
     }
 ?>
