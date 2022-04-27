8f4edca78b2236761cbd5e722ccedc5220370a84
Thales Flores Testoni <thales.testoni@havan.com.br>
2022-04-26 21:11:23 -0300

    Página listagem de livros

---
+++ programacao-internet-2/aula03/ListagemLivros.php
@@ -0,0 +1,51 @@
+<?php
+    include_once 'Estante.php';
+    $estante = new Estante();
+    $listaLivros = $estante->obterListaLivros();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+<body>
+    <div class="vh-100 vw-100 p-5">
+        <h1>Listagem de livros</h1>
+        <table class="table table-hover">
+            <thead>
+                <th scope="col">Nome</th>
+                <th scope="col">Autor</th>
+                <th scope="col">Editora</th>
+                <th scope="col">Detalhes</th>
+            </thead>
+            <tbody>
+                <?php foreach ($listaLivros as $livro): ?>
+                    <tr>
+                        <td><?= $livro->getTitulo(); ?></td>
+                        <td><?= $livro->getAutor(); ?></td>
+                        <td>
+                            <a href=""><?= $livro->getEditora(); ?></a>
+                        </td>
+                        <td>
+                            <a href="">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-text"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
